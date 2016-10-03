a1e4d7cdd703287768605347a63324698830e187
AdityaJ97 <aj7744904623@gmail.com>
2016-10-03 10:17:38 +0530

    dir.c is for -r

--- dir.c
+++ dir.c
@@ -1,30 +1,49 @@
-#include<stdio.h>
-#include<dirent.h>     
-int main(int argc, char *argv[])
- {
-   DIR *dir;
-   dir=opendir(argv[1]);
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <dirent.h>
+#include "header.h"
 
-  struct dirent *dent;
-  if(dir!=NULL)
-   {
-
-       while((dent=readdir(dir)))
-         {
-            FILE *ptr;
-            printf("%s", dent->d_name);
-
-              if(ptr=fopen(dent->d_name,"r"))
-                {
-                     printf("\tFILE\n");
-                     fclose(ptr);
-                }
-              else
-                    printf("\t DIRECTORY\n");
-        }
-           closedir(dir);
-    }
-    else
-            printf("ERROR OPENIN DIRECTORY");
-
+void recursive(char *dname, char *search) {
+	int f[6], size[6], i = 0;
+	char *a;	
+	char s[2] = "\n";
+   	char *token;
+		DIR *dir;
+   		dir=opendir(dname);
+  		struct dirent *dent;
+  		if(dir!=NULL) {
+       			while((dent = readdir(dir))) {
+					if(dent->d_type == DT_DIR) {
+						if(strcmp(dent->d_name, ".") == 0 || strcmp(dent->d_name, "..") == 0);
+						else  {
+                                                        strcat(dname, "/");
+                                                        strcat(dname, dent->d_name);
+                                                        recursive(dname, search);
+                                                }
+					}
+					if(f[i] = open(dent->d_name, O_RDONLY)) { 
+					size[i] = lseek(f[i], 0, SEEK_END);
+					a = (char*)malloc(size[i]*sizeof(char));
+					lseek(f[i], 0, 0);	
+					read(f[i], a, size[i]*sizeof(char));
+   					token = mystrtok(a, s, i);
+ 					while( token != NULL ) { 
+						if(mystrstr( token, search) != NULL) {
+							printf("%s%s/%s%s:%s%s\n", KMAG, dname, dent->d_name, KBLU, KNRM, token);
+						}
+						token = mystrtok(NULL, s, i);
+  	 				}
+				close(f[i]);
+				free (a);
+				}
+			i++;
+			}
+           		closedir(dir);
+	}
 }
