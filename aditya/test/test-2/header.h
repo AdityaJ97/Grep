d4e6c5d890169c77d3a547f6830c2a57f33f85ef
AdityaJ97 <aj7744904623@gmail.com>
2016-10-18 09:06:08 +0530

    Delete header.h

--- aditya/test/test-2/header.h
+++
@@ -1,9 +0,0 @@
-#define KMAG  "\x1B[35m"
-#define KNRM  "\x1B[0m"
-#define KGRN  "\x1B[32m"
-#define KBLU  "\x1B[34m"
-
-void recursive(char *, char *);
-char *mystrstr (char *, char *);
-char *mystrstrcase (char *, char *);
-char *mystrtok (char *, char *, int);
